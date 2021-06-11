f6057e375a37f985f6cea883dff1b52a3aa2dec0
amineAUPEC <79785242+amineAUPEC@users.noreply.github.com>
2021-06-11 14:48:25 +0200

    q3

--- exo2/script1.sh
+++ exo2/script1.sh
@@ -5,15 +5,11 @@ format_search_jpg=$(ls | grep -i ".jpg")
 format_search_jpeg=$(ls | grep -i ".jpeg")
 input_file="./input_file.txt"
 output_file="./output_file.txt"
-nom=$(cat $input_file | cut -f 2 -d ' ' | tr 'A-Z' 'a-z')
-nom_tronque=$(cut -c 1-8 $input_file)
+output_file2="./output_file2.txt"
+nom=$(cat $input_file | cut -f 2 -d ' ' | cut -f 1 -d '|' | tr 'A-Z' 'a-z' > $output_file)
+nom_tronque=$(cut -c 1-8 $output_file)
 # cd $path
 
 rm -rf 
 
-for noms in $nom_tronque
-do
-    echo "$nom_tronque"
-    echo "$nom_tronque" >> $output_file
- 	
-done
+echo "$nom_tronque" > $output_file2
