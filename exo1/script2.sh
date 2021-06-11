1ce74b81bf4751ddf67993de6ec797a4d8b5a272
amineAUPEC <79785242+amineAUPEC@users.noreply.github.com>
2021-06-11 14:22:42 +0200

    q2

---
+++ exo1/script2.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+path="/nfs/opt/bdr/3R-IN3"
+format_search_jpg=$(ls | grep -i ".jpg")
+format_search_jpeg=$(ls | grep -i ".jpeg")
+
+output_file="trouves"
+
+cd $path
+rm -f $output_file
+
+for file_jpg in $format_search_jpg
+do
+    file $file_jpg --extension | grep -i -v 'JPEG'
+ 	
+    if [ $? -eq 0 ]
+        then 
+            echo "$file_jpg is JPG"
+    else 
+            echo "$file_jpg is not JPG" >>  $output_file
+    fi
+done
+
+
+for file_jpeg in $format_search_jpeg
+do
+    file $file_jpeg --extension | grep -i 'JPEG'
+    if [ $? -eq 0 ]
+        then 
+            echo "$file_jpeg is JPEG"
+    else 
+            echo "$file_jpeg is not JPEG" >>  $output_file
+    fi
+
+done
